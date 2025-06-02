295fd29d9f627f5a74fdf733d7ec5179e589e2e2
Donika Yordanova <115694566+donikaiy@users.noreply.github.com>
2025-06-02 13:06:34 +0300

    Delete db_backup directory

--- db_backup/continents_202505152258.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO travel_db.continents (name,image_url) VALUES
-	 ('Europe','https://i.imgur.com/cL2NtHu.png'),
-	 ('Africa','https://i.imgur.com/HsiPAql.png'),
-	 ('North America','https://i.imgur.com/2qaG7VW.png'),
-	 ('Asia','https://i.imgur.com/rBit4NZ.png'),
-	 ('South America','https://i.imgur.com/X2ILZ8L.png'),
-	 ('Australia / Oceania','https://i.imgur.com/X0A5hLP.png');
